869f15b5ab26757f2cb2ee8195132304e56be2dc
wwheeler <wheeler.william@epa.gov>
2021-06-15 02:34:05 +0000

    multiple updates

--- 00 install packages.R
+++ 00 install packages.R
@@ -1,6 +1,8 @@
 ## use renv package to manage R packages, should only have to run once...
 ## routinely get a warning about pacman, even though the package no longer uses pacman...
 
+## vignette on renv https://rstudio.github.io/renv/articles/collaborating.html
+
 renv::hydrate()
 renv::settings$use.cache(FALSE)
 renv::install("geojson")
